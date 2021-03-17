4dc6c3807c0fd15c4cf941af3b6f62dbac3f57ca
a00831789 <77166759+a00831789@users.noreply.github.com>
2021-03-17 11:59:39 -0600

    Juego2 2
    Se modifica función de la comida y el color de la serpiente y comida

--- juego2.py
+++ juego2.py
@@ -1,6 +1,21 @@
-    if head == food:
+ if head == food:
         print('Snake:', len(snake))
-        food.x = randrange(-20, 20) * 10
-        food.y = randrange(-20, 20) * 10
+        food.x = randrange(-18, 18) * 10
+        food.y = randrange(-18, 18) * 10 
     else:
         snake.pop(0)
+
+    clear()
+
+    for body in snake:
+
+        colors = ['#0092fa', '#2ec742', '#000000', '#c72c79', '#f5814c']
+
+        color = colors[randrange(0, 5)]
+
+        square(body.x, body.y, 9, color)
+    
+
+    square(food.x, food.y, 9, color)  
+    update()
+    ontimer(move, 100)
